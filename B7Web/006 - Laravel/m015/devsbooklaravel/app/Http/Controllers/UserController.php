a5b3f644f005cfb11d255c640f0be683f9d6a770
Mayara Silva <devmayara11@gmail.com>
2022-11-08 13:07:46 -0300

    projeto criado

--- B7Web/006 - Laravel/m015/devsbooklaravel/app/Http/Controllers/UserController.php
+++ B7Web/006 - Laravel/m015/devsbooklaravel/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Intervention\Image\Facades\Image;
 
 class UserController extends Controller
 {
@@ -72,6 +73,39 @@ class UserController extends Controller
             }
         }
         $user->save();
+
+        return $array;
+    }
+
+    public function updateAvatar(Request $request)
+    {
+        $array = ['error'=>''];
+        $allowedTypes = ['image/jpg', 'image/jpeg', 'image/png'];
+
+        $image = $request->file('avatar');
+
+        if($image) {
+            if(in_array($image->getClientMimeType(), $allowedTypes)) {
+                $filename = md5(time().rand(0,9999)).'.jpg';
+                $destPath = public_path('/media/avatars');
+
+                $img = Image::make($image->path())
+                ->fit(200, 200)
+                ->save($destPath.'/'.$filename);
+
+                $user = User::find($this->loggedUser['id']);
+                $user->avatar = $filename;
+                $user->save();
+
+                $array['url'] = url('/media/avatars'.$filename);
+            } else {
+                $array['error'] = 'Arquivo não suportado!';
+                return $array;
+            }
+        } else {
+            $array['error'] = 'Arquivo não enviado!';
+            return $array;
+        }
 
         return $array;
     }
