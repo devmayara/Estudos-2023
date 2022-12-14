2ce51314585fd5fcfe964cce7711d73adc3f345f
Mayara Silva <devmayara11@gmail.com>
2022-12-14 09:16:24 -0300

    update

---
+++ YouTube/Yii Framework 2/rest_apis/modules/api/controllers/UsuarioController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\modules\api\controllers;
+
+use yii\rest\ActiveController;
+
+/**
+ * Usuario controller for the `api` module
+ */
+class UsuarioController extends ActiveController
+{
+    public $modelClass = 'app\models\Usuario';
+}
