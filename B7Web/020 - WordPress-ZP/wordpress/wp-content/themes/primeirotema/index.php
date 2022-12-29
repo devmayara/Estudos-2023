71d4b8f353f9e6998d07cf44fe51eec11434b851
Mayara Silva <devmayara11@gmail.com>
2022-12-29 16:53:49 -0300

    Headers, Footers e BodyClass

--- B7Web/020 - WordPress-ZP/wordpress/wp-content/themes/primeirotema/index.php
+++ B7Web/020 - WordPress-ZP/wordpress/wp-content/themes/primeirotema/index.php
@@ -1,20 +1,3 @@
-<html>
-<head>
-    <?php wp_head(); ?>
-</head>
-<body>
-    <h1>Meu Primeiro Tema</h1>
+<?php get_header()?>
 
-    <?php
-        if(has_nav_menu('primary')) {
-            wp_nav_menu(array(
-                'theme_location' => 'primary',
-                'container' => 'nav',
-                'container_class' => 'main_menu',
-                'fallback_cb' => false,
-            ));
-        }
-    ?>
-    <?php wp_footer() ?>
-</body>
-</html>
+<?php get_footer()?>
