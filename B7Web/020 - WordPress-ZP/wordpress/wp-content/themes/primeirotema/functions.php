e4bffa975cb698d47df32c7bdc3161a15a976045
Mayara Silva <devmayara11@gmail.com>
2022-12-29 16:06:14 -0300

    CSS e JS com Hooks

--- B7Web/020 - WordPress-ZP/wordpress/wp-content/themes/primeirotema/functions.php
+++ B7Web/020 - WordPress-ZP/wordpress/wp-content/themes/primeirotema/functions.php
@@ -1,6 +1,8 @@
 <?php
-// Include outros arquivos/functions
-require get_template_directory().'/include/bp_footer_functions.php';
+function bp_theme_styles() {
+    wp_enqueue_style('theme_css', get_template_directory_uri(). '/assets/css/theme.css');
 
-// Hooks
-add_action('shutdown','bp_fim');
+    wp_enqueue_script('theme_js', get_template_directory_uri(). '/assets/js/script.js', array('jquery'), '1.0.0', true);
+}
+
+add_action('wp_enqueue_scripts', 'bp_theme_styles');
--- B7Web/020 - WordPress-ZP/wordpress/wp-content/themes/primeirotema/index.php
+++ B7Web/020 - WordPress-ZP/wordpress/wp-content/themes/primeirotema/index.php
@@ -1,1 +1,9 @@
-<h1>Meu Primeiro Tema</h1>
+<html>
+<head>
+    <?php wp_head(); ?>
+</head>
+<body>
+    <h1>Meu Primeiro Tema</h1>
+    <?php wp_footer() ?>
+</body>
+</html>
