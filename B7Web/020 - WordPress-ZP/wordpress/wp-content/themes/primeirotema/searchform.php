d2487a53cc9aac77cc4a3997859f6e3065c51e19
Mayara Silva <devmayara11@gmail.com>
2022-12-30 09:51:19 -0300

    SearchForm (Barra de pesquisa)

---
+++ B7Web/020 - WordPress-ZP/wordpress/wp-content/themes/primeirotema/searchform.php
@@ -0,0 +1,6 @@
+<form role="search" method="GET" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <input type="search" name="s" value="<?php echo get_search_query(); ?>">
+
+    <input type="submit" value="Buscar">
+</form>
+
