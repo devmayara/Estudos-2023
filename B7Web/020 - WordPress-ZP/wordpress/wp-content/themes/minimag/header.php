06c6818e831e55b175c6a61fe6d600e44863733d
Mayara Silva <devmayara11@gmail.com>
2023-01-02 15:42:41 -0300

    Projeto: Tema do zero: Cabeçalho (3/3)

--- B7Web/020 - WordPress-ZP/wordpress/wp-content/themes/minimag/header.php
+++ B7Web/020 - WordPress-ZP/wordpress/wp-content/themes/minimag/header.php
@@ -50,11 +50,47 @@
                     </div>
                     <div class="main_info">
                         <div class="row">
-                            <div class="col-sm-8">
-...
+                            <div class="col-sm-8 random_post">
+                                <strong>Você já viu?</strong>
+                                <?php
+                                $array = array();
+                                $bm_query = new WP_Query(array(
+                                    'posts_per_page' => 1,
+                                    'post_type' => 'post',
+                                    'orderby' => 'rend'
+                                ));
+                                if($bm_query->have_posts()) {
+                                    while($bm_query->have_posts()) {
+                                        $bm_query->the_post();
+                                        ?>
+                                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                        <?php
+                                    }
+                                    wp_reset_postdata();
+                                }
+                                ?>
                             </div>
                             <div class="col-sm-4 social_area">
-...
+                                <div class="social_txt">
+                                    SIGA:
+                                </div>
+                                <div class="social_icons">
+                                    <a href="https://facebook.com" target="_blank">
+                                        <img src="<?php echo get_template_directory_uri().'/assets/images/facebook.png'; ?>">
+                                    </a>
+                                    <a href="https://google.com" target="_blank">
+                                        <img src="<?php echo get_template_directory_uri().'/assets/images/gplus.png'; ?>">
+                                    </a>
+                                    <a href="https://instagram.com" target="_blank">
+                                        <img src="<?php echo get_template_directory_uri().'/assets/images/instagram.png'; ?>">
+                                    </a>
+                                    <a href="https://twitter.com" target="_blank">
+                                        <img src="<?php echo get_template_directory_uri().'/assets/images/twitter.png'; ?>">
+                                    </a>
+                                    <a href="https://youtube.com" target="_blank">
+                                        <img src="<?php echo get_template_directory_uri().'/assets/images/youtube.png'; ?>">
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
