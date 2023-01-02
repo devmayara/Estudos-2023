04e992e42cf65c004f50aa340e66e4365ace54f0
Mayara Silva <devmayara11@gmail.com>
2023-01-02 14:13:25 -0300

    Projeto: Tema do zero: Cabeçalho (1/3)

--- B7Web/020 - WordPress-ZP/wordpress/wp-content/themes/minimag/header.php
+++ B7Web/020 - WordPress-ZP/wordpress/wp-content/themes/minimag/header.php
@@ -2,11 +2,53 @@
 <html lang="en">
 <head>
     <?php wp_head(); ?>
-
-    <!-- <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title> -->
 </head>
-<body>
-    hello
+<body <?php body_class(); ?>>
+    <header>
+        <div class="top_header">
+            <nav class="navbar navbar-expand-md">
+                <div class="container-fluid">
+                    <!-- <div class="collapse navbar-collapse"> -->
+                        <?php 
+                            if(has_nav_menu('top')) {
+                                wp_nav_menu(array(
+                                    'theme_location' => 'top',
+                                    'container' => false,
+                                    'fallback_cb' => false,
+                                    'menu_class' => 'navbar-nav mr-auto'
+                                ));
+                            }
+                        ?>
+                    <!-- </div> -->
+                </div>
+            </nav>
+        </div>
+        <div class="main_header">
+            <div class="container-fluid">
+                <div class="logo">
+                    <?php
+                        if(has_custom_logo()) {
+                            the_custom_logo();
+                        }
+                    ?>
+                </div>
+                <div class="main_nav_border">
+                    <div class="main_nav">
+                        <?php 
+                            if(has_nav_menu('primary')) {
+                                wp_nav_menu(array(
+                                    'theme_location' => 'primary',
+                                    'container' => false,
+                                    'fallback_cb' => false,
+                                    'menu_class' => 'navbar-nav bd-navbar-nav flex-row'
+                                ));
+                            }
+                        ?>
+                    </div>
+                    <div class="main_info">
+                        .
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
--- B7Web/020 - WordPress-ZP/wordpress/wp-content/themes/minimag/include/setup.php
+++ B7Web/020 - WordPress-ZP/wordpress/wp-content/themes/minimag/include/setup.php
@@ -1,22 +1,19 @@
 <?php
-
 function bm_theme_styles() {
     wp_enqueue_style('bootstrap_css', get_template_directory_uri().'/assets/css/bootstrap.min.css');
     wp_enqueue_style('template_css', get_template_directory_uri().'/assets/css/template.css');
 
-    wp_enqueue_script('bootstrap_js', get_template_directory_uri().'/assets/css/bootstrap.min.js');
-    wp_enqueue_script('script_js', get_template_directory_uri().'/assets/css/script.js');    
+    wp_enqueue_script('bootstrap_js', get_template_directory_uri().'/assets/js/bootstrap.min.js');
+    wp_enqueue_script('script_js', get_template_directory_uri().'/assets/js/script.js');
 }
-
 function bm_after_setup() {
     add_theme_support("post-thumbnails");
     add_theme_support("title-tag");
     add_theme_support("custom-logo");
 
-    register_nav_menus("primary", "Menu Primário");
-    register_nav_menus("top", "Menu Superior");
+    register_nav_menu("primary", "Menu Primário");
+    register_nav_menu("top", "Menu Superior");
 }
-
 function bm_widgets() {
 
 }
