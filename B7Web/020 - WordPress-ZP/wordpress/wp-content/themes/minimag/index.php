4493687baa05885d368b78e8d8e21d02824e1c9d
Mayara Silva <devmayara11@gmail.com>
2023-01-05 08:29:56 -0300

    Projeto: Tema do zero: Home

--- B7Web/020 - WordPress-ZP/wordpress/wp-content/themes/minimag/index.php
+++ B7Web/020 - WordPress-ZP/wordpress/wp-content/themes/minimag/index.php
@@ -4,7 +4,24 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-8 main_content">
-                ...
+                <?php if(have_posts()): ?>
+                    <?php while(have_posts()): ?>
+                        <?php the_post(); ?>
+
+                        <?php get_template_part( 'template_parts/post'); ?>
+
+                    <?php endwhile; ?>
+
+                    <div class="pag">
+                        <div class="previous_pag">
+                            <?php previous_posts_link('Página Anterior'); ?>
+                        </div>
+                        <div class="naxt_pag">
+                            <?php next_posts_link('Próxima Página'); ?>
+                        </div>
+                    </div>
+
+                <?php endif; ?>
             </div>
             <?php get_sidebar(); ?>
         </div>
